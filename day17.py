ddcbe3ccde1c8920c7ba458ab1b1bfcee212b4cb
unknown <NeerajKatheriya1435>
2025-05-31 18:59:50 +0530

    dictionary explained

--- day17.py
+++ day17.py
@@ -14,12 +14,64 @@
 # mySet.update([8,9,56,788])
 # print(mySet)
 
-a = {1, 2, 3} # a union b ==>{1,2,3,4}
-b = {2, 3, 4} 
+# a = {1, 2, 3} # a union b ==>{1,2,3,4}  
+# b = {2, 3, 4} # intersection ==> {2,3}
 # tup=(7,)
 # asdf={7,8,3,4}
 # print(type(asdf))
 
+# a-b==>  {1}
+# union=a.union(b)
+# inter=a.intersection(b)
+# diff=a.difference(b)
+# diff=a.symmetric_difference(b)
+# print(diff)
+
+# set1={8,5,4}
+
+# if 18 in set1:
+#     print("Yes")
+# else:
+#     print("No")
+
+# student = {'name': 'John', 'age': 21, 'course': 'Python'}
 
 
+# print(dict1)
+# print(dict1["Age"])
+# print(dict1["Name1"])
+# print(dict1.get("Name1"))
+# print(dict1.keys())
+# print(dict1.values())
+# print(dict1.items())
 
+# for item in dict1.keys():
+#     print(item)
+# for item1 in dict1.values():
+#     print(item1)
+# for key,value in dict1.items():
+#     print(f"the key is {key} and value is {value}")
+
+dict1={
+    "Name":"Ayush",
+    "Age":26,
+    "Course":"B.sc"
+}
+
+# dict2={
+#     101:"Ayush",
+#     102:"Shubham"
+# }
+
+# dict1.update(dict2)
+
+# dict3=dict1.copy()
+# print(dict3)
+
+# dict1.pop("Name")
+# dict1.popitem()
+
+# dict1.clear()
+del dict1
+
+# print(dict1)
--- day9.py
+++ day9.py
@@ -1,4 +1,4 @@
-inp1=int(input("Enter number for table: "))
+# inp1=int(input("Enter number for table: "))
 # i=1
 # while(i<11):
     
@@ -6,8 +6,23 @@ inp1=int(input("Enter number for table: "))
 #     print(inp1,"X",i,"=",answer)
 #     i=i+1
 
-for i in range(1,11):
+# for i in range(1,11):
     # print(i+1)
     # print(i)
-    answer=i*inp1
-    print(inp1,"X",i,"=",answer)
+    # answer=i*inp1
+    # print(inp1,"X",i,"=",answer)
+# a=824
+# b=8245
+# print("A1") if(a>b) else print("Hello") if(a==b) else print("No111") if(a<b) else print("No") if(a&b) else print("No")
+
+# print(9 if(a>b) else "A123")
+
+list1={
+    "hello":"umar",
+    56:89,
+    "suman":"like"
+}
+
+for index,item in enumerate(list1):
+    if(index==2):
+        print(item)
