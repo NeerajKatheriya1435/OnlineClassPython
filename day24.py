65f80444c14c975f3bc106c520f66d29fe988546
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-06-19 18:33:14 +0530

    Lambda Function and map

---
+++ day24.py
@@ -0,0 +1,55 @@
+f=open("data.txt","r+")
+
+# while True:
+#     line=f.readline()
+#     if not line:
+#         break
+#     print(line)
+
+# print("This will execute if code is break")
+
+# f.write("Hello sir kaise hai Ayush")
+# lst12=["Hello Durgesh\n","Hii Sir\n","Kaise ho yeh btaio"]
+
+# f.writelines(lst12)
+
+# f.seek(5)
+# content=f.readline()
+# content=f.readline()
+# count=f.tell()
+# f.write("How are you shivam")
+
+# f.truncate(8)
+# content=f.read()
+# print(content)
+# f.close()
+
+# add=lambda a,b:a+b
+
+# def add(a,b):
+#     return a+b
+# print(add(6,7))
+
+# cube=lambda x:x*x*x
+# print(cube(6))
+
+# newFunc=lambda a,b,c:(a*b)+c
+
+# print(newFunc(2,3,6))
+
+# def greet():
+#     print("Hello Good Morning")
+
+# def dusraFunc(greetAyush):
+#     print("Hello Guys")
+#     print(greetAyush(6))
+
+# greet=lambda x:x*x
+# dusraFunc(greet)
+
+# list1=[8,5,4,2]
+# def func1(a):
+#     return a+5
+
+# square=list(map(func1,list1))
+# print(square)
